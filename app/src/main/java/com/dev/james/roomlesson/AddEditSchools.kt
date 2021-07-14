e3d25b1b39915cb10b582937afb741b6e7260b97
James Gitonga <40741293+JayExtra@users.noreply.github.com>
2021-07-14 20:44:30 +0300

    Initial commit

---
+++ app/src/main/java/com/dev/james/roomlesson/AddEditSchools.kt
@@ -0,0 +1,11 @@
+package com.dev.james.roomlesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditSchools : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_schools)
+    }
+}
